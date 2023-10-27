10cf76eb7debb73d25f9f5bd04dd0f7aec7e4866
yellowandrew <68777254@qq.com>
2023-10-27 11:08:15 +0800

    init project

---
+++ GameProtos/Assets/Prototypes/Commons/Messanger.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Messanger 
+{
+    static Dictionary<int, List<EventHandler<object>>> dicts = new Dictionary<int, List<EventHandler<object>>>();
+    public static void Emit(int mId, object msg, object sender = null) {
+        foreach (var handler in dicts[mId])
+        {
+            handler.Invoke(sender, msg);
+        }
+    }
+
+    public static void Bind(int mId, EventHandler<object> handler) {
+        List<EventHandler<object>> list;
+        if (dicts.ContainsKey(mId))
+        {
+            list = dicts[mId];
+        }
+        else
+        {
+            list = new List<EventHandler<object>>();
+            dicts.Add(mId, list);
+        }
+        list.Add(handler);
+    }
+
+    public static void Delete(int mId, EventHandler<object> handler) {
+        if (dicts.ContainsKey(mId))
+        {
+            foreach (var h in dicts[mId])
+            {
+                if (h == handler)
+                {
+                    dicts[mId].Remove(h);
+                }
+            }
+        }
+    }
+   
+}
